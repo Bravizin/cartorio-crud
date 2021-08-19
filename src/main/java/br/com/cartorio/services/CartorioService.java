2a10c720f4f02ccfa1cd7f72ce1b9232cbc2e90b
Leonardo Selmes <leo_rse@hotmail.com>
2021-08-19 17:30:31 -0300

    teste

--- src/main/java/br/com/cartorio/services/CartorioService.java
+++ src/main/java/br/com/cartorio/services/CartorioService.java
@@ -16,6 +16,7 @@ public class CartorioService {
 	private CartorioRepository repository;
 
 	public List<Cartorio> listarCartorios() {
+		String teste = "teste";
 		return repository.findAll();
 	}
 
